e8a81d70cc72b89f0a1a12c9af49d44a83e241f2
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-05-05 22:19:33 +0200

    fixed p typo

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = class Texter extends Plugin {
       usage: '{c} [ZWS]',
       executor: (args) => ({
         send: true,
-        result: '\u200b' + args.join(' ').split('').join('\u200b') + '\u200bP',
+        result: '\u200b' + args.join(' ').split('').join('\u200b') + '\u200b',
       }),
     });
 
